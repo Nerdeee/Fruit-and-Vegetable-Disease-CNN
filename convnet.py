30f6ddec59576651c99f697d3c351acd8dbf0d74
Nerdeee <george.larres@gmail.com>
2024-07-03 22:05:39 -0700

    Changed learning rate to improve model accuracy

--- convnet.py
+++ convnet.py
@@ -78,7 +78,7 @@ class NeuralNet(nn.Module):
 model = NeuralNet()
 
 loss_fn = nn.CrossEntropyLoss()    
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.005)
 
 # training
 num_epochs = 100
