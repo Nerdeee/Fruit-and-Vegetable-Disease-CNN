119cc7640eeffdad8f4835f34414f35decfd4977
Nerdeee <george.larres@gmail.com>
2024-07-28 18:38:21 -0700

    Trying new learning rate

--- convnet.py
+++ convnet.py
@@ -7,7 +7,7 @@ import os
 from torch.utils.tensorboard import SummaryWriter
 
 fully_connected_layers = 4
-learning_rate = 0.0008
+learning_rate = 0.0007
 dropout_probability = 0.1
 experiment_name = f'LearningRate_{learning_rate} DropoutProbability_{dropout_probability} FCL_{fully_connected_layers}'
 experiment_dir_name = os.path.join('runs', experiment_name)
